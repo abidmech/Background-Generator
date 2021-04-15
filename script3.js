2613e67becd3325bbda4bc791aec57f08118675b
Abid Hussain <abidhussain78657@gmail.com>
2021-04-15 14:23:13 +0530

    firstcommit

---
+++ script3.js
@@ -0,0 +1,23 @@
+var css=document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var body=document.querySelector("body");
+
+function setGradient(){
+	var grad = "linear-gradient(to right," 
+	+color1.value 
+	+", "
+	+color2.value 
+	+ ")";
+	body.setAttribute("style","background:"+grad)
+
+}
+
+
+
+
+color1.addEventListener("input",setGradient);
+	
+	
+color2.addEventListener("input",setGradient);
+	v
